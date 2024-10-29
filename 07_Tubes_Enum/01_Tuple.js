59eac8b380ed314414891efa2bc460679cbbec2e
Selva_lr <84980271+selvalr@users.noreply.github.com>
2024-10-29 12:06:51 +0000

    TypeScript tutorial

---
+++ 07_Tubes_Enum/01_Tuple.js
@@ -0,0 +1,14 @@
+// ----------------------
+var myTuple;
+myTuple = [2, "Hello World"]; // Valid🥂
+// myTuple = ["Hello World", 2]; // Not Valid ⚠️
+console.log(myTuple);
+// ----------------------
+// ----------------------
+var products = ["Item 1", 23];
+console.log(products);
+// ----------------------
+// ----------------------
+var games = ["Game 1", "Game 2", "Game 3"];
+console.log(games);
+// ----------------------
---
+++ 07_Tubes_Enum/02_Enum.js
@@ -0,0 +1,9 @@
+var WeatherConditions;
+(function (WeatherConditions) {
+    WeatherConditions["Sunny"] = "sunny";
+    WeatherConditions["Cloudy"] = "cloudy";
+    WeatherConditions["Rainy"] = "rainy";
+    WeatherConditions["Snowy"] = "snowy";
+})(WeatherConditions || (WeatherConditions = {}));
+var currentWeather = WeatherConditions.Sunny;
+console.log("The current weather is ".concat(currentWeather));
